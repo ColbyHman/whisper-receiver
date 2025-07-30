3ba865b4e565a688d252f1fe2558509d58f8a40f
Colby Hillman <colby.hillman@gmail.com>
2025-07-29 22:50:53 -0400

    Changed arg to file_path

--- src/transcriber/helpers/transcriber.py
+++ src/transcriber/helpers/transcriber.py
@@ -2,10 +2,7 @@ import tempfile
 
 async def transcribe(file_path, model) -> dict:
 
-    with open(file_path, "rb") as f:
-        audio_bytes = f.read()
+    # Run whisper transcription
+    result = model.transcribe(file_path)
 
-        # Run whisper transcription
-        result = model.transcribe(audio_bytes)
-
-        return result
+    return result
