e604da27bff0f42968c37c4341347a3d53830502
Colby Hillman <colby.hillman@gmail.com>
2025-11-04 22:29:10 -0500

    Added redis, initial setup

---
+++ src/transcriber/adapters/redis.py
@@ -0,0 +1,18 @@
+from redis import Redis
+
+def get_redis_client(host: str = "localhost", port: int = 6379, db: int = 0) -> Redis:
+    """Returns Redis Client"""
+
+    try:
+        client = Redis(host=host, port=port, db=db)
+        client.ping()
+        return client
+    except Exception as e:
+        raise Exception("Could not get Redis Client: ", e)
+    
+def insert(key: str, value: str, client: Redis):
+
+    try:
+        client.lpush(key, value)
+    except Exception as e:
+        raise Exception("Could not insert item: ", e)
